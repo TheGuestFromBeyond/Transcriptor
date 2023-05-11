7dfbee128aff7572ab787b9cbb35d7a7334ebb30
TheGuestFromBeyond <theguestfrombeyond@protonmail.com>
2023-05-11 12:37:01 +0300

    first commit

---
+++ src/main/java/com/beyond/transcriptor/TranscriptorApplication.java
@@ -0,0 +1,13 @@
+package com.beyond.transcriptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranscriptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranscriptorApplication.class, args);
+	}
+
+}
